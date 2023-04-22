d17f23d18cebd658c2221dc7d630edd97f370bd6
Joseph <sejokarizz@gmail.com>
2023-04-22 13:25:06 +0300

    check betty compliance

--- _printf.c
+++ _printf.c
@@ -1,47 +1,50 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
 
 /**
  * _printf - prints output according to a format
  * @format: pointer to a format string
+ *
  * Return: number of characters printed
  **/
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0, i = 0;
+va_list args;
+int count = 0;
+char *str;
 
-	va_start(args, format);
-	for (; format && format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == 'c')
-			{
-				_putchar(va_arg(args, int)), count++;
-			}
-			else if (format[i] == 's')
-			{
-				char *s = va_arg(args, char *);
-
-				s = (!s) ? "(null)" : s;
-				while (*s)
-				{
-					_putchar(*s++), count++;
-				}
-			}
-			else if (format[i] == '%')
-			{
-				_putchar('%'), count++;
-			}
-		}
-		else
-		{
-			_putchar(format[i]), count++;
-		}
-	}
-	va_end(args);
-	return (count);
+va_start(args, format);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+_putchar(va_arg(args, int));
+count++;
+}
+else if (*format == 's')
+{
+str = va_arg(args, char *);
+while (*str)
+{
+_putchar(*str++);
+count++;
+}
+}
+else if (*format == '%')
+{
+_putchar('%');
+count++;
+}
+}
+else
+{
+_putchar(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
 }
