44786e90c6b2fe00cf506f89b957e5da568e8dc1
Joseph <sejokarizz@gmail.com>
2023-04-22 16:00:48 +0300

    break function itno smaller function.

--- _printf.c
+++ _printf.c
@@ -1,50 +1,66 @@
-#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * _printf - prints output according to a format
- * @format: pointer to a format string
+ * _printf - produces output according to a format
+ * @format: format string containing directives
  *
  * Return: number of characters printed
- **/
+ */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
-char *str;
+	va_list args;
+	int i, count = 0;
 
-va_start(args, format);
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-if (*format == 'c')
-{
-_putchar(va_arg(args, int));
-count++;
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					/* code to print a character */
+					break;
+				case 's':
+					count += print_str(va_arg(args, char *));
+					break;
+				case '%':
+					/* code to print a percent sign */
+					break;
+				default:
+					/* code to handle invalid conversion specifiers */
+					break;
+			}
+		}
+		else
+		{
+			count += putchar(format[i]);
+		}
+	}
+
+	va_end(args);
+
+	return (count);
 }
-else if (*format == 's')
+
+/**
+ * print_str - prints a string to the standard output stream
+ * @str: pointer to the string to print
+ *
+ * Return: number of characters printed
+ */
+int print_str(char *str)
 {
-str = va_arg(args, char *);
-while (*str)
-{
-_putchar(*str++);
-count++;
-}
-}
-else if (*format == '%')
-{
-_putchar('%');
-count++;
-}
-}
-else
-{
-_putchar(*format);
-count++;
-}
-format++;
-}
-va_end(args);
-return (count);
+	int i, count = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	for (i = 0; str[i] != '\0'; i++)
+		count += putchar(str[i]);
+
+	return (count);
 }
