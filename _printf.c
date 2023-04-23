63f2352f53dc7bca0e24ac0c38553ccf6532ad3b
Joseph <sejokarizz@gmail.com>
2023-04-24 00:43:30 +0300

    task 0 complete test3

--- _printf.c
+++ _printf.c
@@ -1,41 +1,34 @@
 #include "main.h"
 
-/**
- * _printf - produces output according to a format
- * @format: format string containing directives
- *
- * Return: number of characters printed
- */
-int _printf(const char *format, ...)
+int  my_printf(const char *format, ...)
 {
+	unsigned j = 0, return_value = 0;
 	va_list args;
-	int counter = 0, j = 0;
-
 	va_start(args, format);
 
-	for (; format[j] != '\0'; j++)
+	for ( ; format[j] != '\0' ; j++)
 	{
-
 		if (format[j] != '%')
 		{
-			_putchar(format[j]);
-			counter++;
+			putchr(format[j]);
 		}
-		else if (format[j + 1] == 'c')
+		else if (format[j+1] == 'c')
 		{
-			_putchar(va_arg(args, int));
+			putchr(va_arg(args, int));
 			j++;
-			counter++;
 		}
-		else if (format[j + 1] == 's')
+		else if (format[j+1] == 's')
 		{
-			int return_value = puts_string(va_arg(args, char*));
-
+			int r_val = puts_string(va_arg(args, char *));
 			j++;
-			counter += return_value;
+			return_value += (r_val - 1);
 		}
+		else if (format[j+1] == '%')
+		{
+			putchr('%');
+			j++;
+		}
+		return_value += 1;
 	}
-	va_end(args);
-
-	return (counter);
+	return (return_value);
 }
