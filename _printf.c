73e983cd74e201934a727c64e0e59079d3e8ee2a
Joseph <sejokarizz@gmail.com>
2023-04-24 00:56:52 +0300

    task 0 complete test5

--- _printf.c
+++ _printf.c
@@ -1,34 +1,37 @@
 #include "main.h"
 
-int  my_printf(const char *format, ...)
+int my_printf(const char *format, ...)
 {
-	unsigned j = 0, return_value = 0;
-	va_list args;
-	va_start(args, format);
+unsigned int j = 0;
+int rt_value = 0;
+va_list args;
+va_start(args, format);
 
-	for ( ; format[j] != '\0' ; j++)
-	{
-		if (format[j] != '%')
-		{
-			_putchar(format[j]);
-		}
-		else if (format[j+1] == 'c')
-		{
-			_putchar(va_arg(args, int));
-			j++;
-		}
-		else if (format[j+1] == 's')
-		{
-			int r_val = puts_string(va_arg(args, char *));
-			j++;
-			return_value += (r_val - 1);
-		}
-		else if (format[j+1] == '%')
-		{
-			_putchar('%');
-			j++;
-		}
-		return_value += 1;
-	}
-	return (return_value);
+for (; format[j] != '\0'; j++)
+{
+
+if (format[j] != '%')
+{
+_putchar(format[j]);
+}
+else if (format[j + 1] == 'c')
+{
+_putchar(va_arg(args, int));
+j++;
+}
+else if (format[j + 1] == 's')
+{
+int r_val = puts_string(va_arg(args, char *));
+j++;
+rt_value += (r_val - 1);
+}
+else if (format[j + 1] == '%')
+{
+_putchar('%');
+j++;
+}
+rt_value += 1;
+}
+va_end(args);
+return (rt_value);
 }
