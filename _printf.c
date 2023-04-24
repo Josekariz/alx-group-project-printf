93647fe1132909e6cf9c0850cb789886a94b7689
Joseph <sejokarizz@gmail.com>
2023-04-24 10:01:58 +0300

    test 1

--- _printf.c
+++ _printf.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
+
 
 /**
  * _printf - prints output according to a format
--- main.h
+++ main.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+
 #include <stdarg.h>
 #include <stddef.h>
 
